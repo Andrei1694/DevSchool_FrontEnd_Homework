9d2d16ead9b3572a1bce674afe541822797b4fa3
Andrei1694 <andrei4.stanciu@gmail.com>
2018-11-18 06:02:37 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 window.onscroll = function() {myFunction()};
 
+// Get the navbar
 var navbar = document.getElementById("navbar");
 
-
+// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
@@ -13,3 +14,56 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 } 
+
+var numSteps = 20.0;
+
+var boxElement;
+var prevRatio = 0.0;
+var increasingColor = "rgba(40, 40, 190, ratio)";
+var decreasingColor = "rgba(190, 40, 40, ratio)";
+
+// Set things up.
+
+window.addEventListener("load", function(event) {
+  boxElement = document.querySelector("w");
+
+  createObserver();
+}, false);
+
+function createObserver() {
+  var observer;
+
+  var options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: buildThresholdList()
+  };
+
+  observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(boxElement);
+}
+
+function buildThresholdList() {
+  var thresholds = [];
+  var numSteps = 20;
+
+  for (var i=1.0; i<=numSteps; i++) {
+    var ratio = i/numSteps;
+    thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  return thresholds;
+}
+
+function handleIntersect(entries, observer) {
+  entries.forEach(function(entry) {
+    if (entry.intersectionRatio > prevRatio) {
+      entry.target.style.backgroundColor = increasingColor.replace("ratio", entry.intersectionRatio);
+    } else {
+      entry.target.style.backgroundColor = decreasingColor.replace("ratio", entry.intersectionRatio);
+    }
+
+    prevRatio = entry.intersectionRatio;
+  });
+}
